93bc7517e32dc73cb8a928f90d250b703ad7399e
bhavanafokmare <138211356+bhavanafokmare@users.noreply.github.com>
2023-06-30 16:30:26 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,33 @@
+# firstly i make a list by using int, float,slice,string etc
+# print('=====My favourite Movie Collection====')
+# print('===English Movie===')
+# M = []
+# M.append('Forest Gum')
+# print(id(M))
+# M.append('A Beautiful Mind')
+# print(id(M))
+# M.append('Life Of Pi')
+# print(id(M))
+# M.append('Venome')
+# print(id(M))
+# print(M)
+# # i add again two movie
+# M.append('Taare Jamin Per')
+# print(id(M)) #with same id
+# M.append('Mission Mangal')
+# print(M)
+# length of the M
+# print('Length of the M:',len(M))
+# #Reverse with the help of slice
+# print(M[ : :-1])
+# #indexing
+# print ( 'At index 0 :',M[0])
+# print('At index 1 :',M[1])
+# print('At index 2 :',M[2])
+# print('At index -3 :',M[-3])
+# print('At index 4 :',M[4])
+# print('At index -1',M[-1])
+
+
+
+
